840252556706c25acc99b51edc95d866331b688e
Irhas <muhammadirhasalh@gmail.com>
2023-10-15 17:53:30 +0700

    Added currency code picker

--- lib/src/pages/currency_page.dart
+++ lib/src/pages/currency_page.dart
@@ -1,9 +1,18 @@
 import '../common.dart';
+import '../currency_code.dart';
 
-class CurrencyPage extends StatelessWidget {
+class CurrencyPage extends StatefulWidget {
   static const routeName = '/currency';
 
   const CurrencyPage({super.key});
+
+  @override
+  State<CurrencyPage> createState() => _CurrencyPageState();
+}
+
+class _CurrencyPageState extends State<CurrencyPage> {
+  String _code = 'IDR';
+  String _name = 'Indonesia';
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +21,42 @@ class CurrencyPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(l10n.setCurrency),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => _showCurrencyCodePicker(),
+            title: Text(_code),
+            subtitle: Text(_name),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showCurrencyCodePicker() {
+    final nav = Navigator.of(context);
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        child: Column(
+          children: List.generate(
+            kCurrencyCode.length,
+            (index) => ListTile(
+              onTap: () {
+                setState(() {
+                  _code = '${kCurrencyCode[index]['code']}';
+                  _name = '${kCurrencyCode[index]['name']}';
+                });
+
+                nav.pop();
+              },
+              title: Text('${kCurrencyCode[index]['code']}'),
+              subtitle: Text('${kCurrencyCode[index]['name']}'),
+            ),
+          ),
+        ),
       ),
     );
   }
