5b3e92ad2d9dc816f6bd042593a386a824f50f4e
Irhas <muhammadirhasalh@gmail.com>
2023-11-21 05:29:43 +0700

    Updated environment & dependencies

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 const kSeedColor = Color(0xff004af6);
 
-final theme = ThemeData.light(useMaterial3: true).copyWith(
+final theme = ThemeData.light().copyWith(
     colorScheme: ColorScheme.fromSeed(
   seedColor: kSeedColor,
   brightness: Brightness.light,
 ));
 
-final darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
+final darkTheme = ThemeData.dark().copyWith(
     colorScheme: ColorScheme.fromSeed(
   seedColor: kSeedColor,
   brightness: Brightness.dark,
